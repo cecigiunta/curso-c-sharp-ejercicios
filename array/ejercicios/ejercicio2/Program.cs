8d41e39d1bb2dc1d8dfeea8baa464606d4d3876d
cecigiunta <ceciliagiunta57@gmail.com>
2022-08-06 00:38:54 -0300

    ejercicio2 array

--- array/ejercicios/ejercicio2/Program.cs
+++ array/ejercicios/ejercicio2/Program.cs
@@ -1,4 +1,4 @@
-﻿//* Hacer un programa que solicite 10  números enteros y los guarde en un vector.
+﻿//* Hacer un programa que solicite 6 números enteros y los guarde en un vector.
 //* Luego recorrer ese vector para calcular el promedio. Mostrar por pantalla los valores que son mayores al promedio.
 
 using System;
@@ -9,7 +9,23 @@ namespace ejercicio2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] numeros = new double [6];
+            double num, mayores = 0, acumulador = 0;
+            double promedio;
+
+            for(int i = 0; i< 6; i++){
+                Console.WriteLine("Ingrese un numero");
+                num = int.Parse(Console.ReadLine());
+                numeros[i]= num;
+                acumulador += num;
+            }
+            promedio = (acumulador / 6);
+            Console.WriteLine("El promedio es: "+ promedio);
+            for(int j = 0 ; j < 6; j++){
+                if(numeros[j] > promedio){
+                    Console.WriteLine("El numero "+ numeros[j] + " es mayor al promedio");
+                }
+            }
         }
     }
 }
