88ed1f541ecd31e43a0f2e6670dfbc9f1b1cd19d
cecigiunta <ceciliagiunta57@gmail.com>
2022-08-08 02:29:52 -0300

    ejercicio 3: Reemplazo de caracteres

--- CadenaCaracteres/ejercicios/reemplazoCaracter/Program.cs
+++ CadenaCaracteres/ejercicios/reemplazoCaracter/Program.cs
@@ -12,8 +12,72 @@ namespace reemplazoCaracter
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            /*
+                RESOLUCIÓN LARGA: 
+            char letra1, letra2;
+            char[] frase = new char [30];
+            int indice = 0;
+
+            char letra;
+
+            Console.WriteLine("Ingrese una letra");
+            letra = char.Parse(Console.ReadLine());
+            while (letra != '0' && indice < 30)
+            {
+                frase[indice] = letra;
+                Console.WriteLine("Ingrese otra letra");
+                letra = char.Parse(Console.ReadLine());
+                indice++; //Incrementa para q la letra se vaya guardando en c indice
+                
+            }
+            frase[indice] = '\0';  //Determinamos con \0 el final de la cadena
+            Console.WriteLine("La frase es: " );
+            indice = 0;
+
+            while (frase[indice] != '\0'){
+                Console.Write(frase[indice]);
+                indice++;
+            }
+
+            Console.WriteLine("Ingrese letra a reemplazar: ");
+            letra1 = char.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la letra nueva");
+            letra2 = char.Parse(Console.ReadLine());
+
+            indice = 0; 
+
+            while (frase[indice] != '\0')
+            {
+                if(frase[indice] == letra1){  //Voy a recorrer la cadena y cada vez q encuentre la letra, la reemplazo por la otra
+                    frase[indice] = letra2;
+                    indice++;
+                }
+                Console.WriteLine("La frase nueva es :" );
+                indice = 0;
+                while(frase[indice] != '\0'){
+                    Console.Write(frase[indice]);
+                    indice++;
+                }
+            }
+            */
+
+
+
+            //* RESOLUCIÓN CORTA CON STRING Y MÉTODO REPLACE
+            string frase;
+            char letra1, letra2;
+            Console.WriteLine("Ingrese la frase: ");
+            frase = Console.ReadLine();
+            Console.WriteLine("Ingrese la letra a reemplazar: ");
+            letra1 = char.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la letra nueva");
+            letra2 = char.Parse(Console.ReadLine());
+
+            frase = frase.Replace(letra1, letra2);  //metodos de string
+
+            Console.WriteLine("La cadena nueva es: " );
+            Console.WriteLine(frase);
         }
     }
 }
