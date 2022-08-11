a45b1d4cc2b2dae29633a407d11147baf920cef1
cecigiunta <ceciliagiunta57@gmail.com>
2022-08-11 01:49:15 -0300

    ej 4 Vectores

--- array/ejercicios/registroArticulos/Program.cs
+++ array/ejercicios/registroArticulos/Program.cs
@@ -16,7 +16,55 @@ namespace registroArticulos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Entra el concepto de REGISTRO: 
+            //Algo que esta compuesto por más de una variable.
+            // Ej "Venta": constituida por fecha, item, cliente, producto
+
+            int numArticulo, cantVendida;
+            int [] totalCantidadVendida = new int[15]; //Cantidad de cada uno de los art
+
+            for(int i = 0; i < totalCantidadVendida.length ; i++){
+                totalCantidadVendida[i] = 0; //Inicializa en 0 todos lo elementos
+            }
+
+            //Pido el registro
+            Console.WriteLine("Ingrese el numero de articulo: ");
+            numArticulo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad vendida: ");
+            cantVendida = int.Parse(Console.ReadLine());
+
+            while(numArticulo != 0){ //Mientras que sea distinto de 0... sigo pidiendo registro
+            totalCantidadVendida[numArticulo -1] += cantidad;  //numArticulo -1 por la posicion
+                //le incremento a ese indice la cantidad en 1
+    
+            Console.WriteLine("Ingrese el numero de articulo: ");
+            numArticulo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad vendida: ");
+            cantVendida = int.Parse(Console.ReadLine());
+
+            }
+
+            //Punto A: tengo q buscar el maximo del vector y su posicion (indice + 1: el art)
+            int maxCantidad = totalCantidadVendida[0];   //Asigno el maximo en el primer elemento para arrancar
+            int numMaximo = 1;   //Asigno el numero de art. en 1 para arrancar
+            for(int i = 0 ; i < totalCantidadVendida.length ; i++){
+                if (totalCantidadVendida[i] > maxCantidad) {   
+                    maxCantidad = totalCantidadVendida[i];     //Le asigno el actual
+                    numArticulo = i + 1;     //Le digo q mi articulo va a ser ese + 1 
+                }
+            }
+            Console.WriteLine("el producto mas vendido es el: " + numArticulo + " Con la cantidad de: " + maxCantidad);
+        
+        
+            //Punto B: Numero de los productos q no tienen ventas
+            for (int i = 0; i < totalCantidadVendida.length ; i++){
+                if(totalCantidadVendida[x] == 0){   //Si alguno del array de cantidades tiene 0...
+                    Console.WriteLine("El producto " + (x+1) + " no registró ventas");
+                }
+            }
+
+            //Punto C: Cuantos hay del punto C
+            Console.WriteLine("La cantidad del 10 es: " + totalCantidadVendida[9]);   
         }
     }
 }
