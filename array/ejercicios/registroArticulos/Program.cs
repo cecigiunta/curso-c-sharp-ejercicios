f22b3b69338aa2f8eed69b4249d20e74230ba864
cecigiunta <ceciliagiunta57@gmail.com>
2022-08-23 14:20:29 -0300

    ej funcion que mult

--- array/ejercicios/registroArticulos/Program.cs
+++ array/ejercicios/registroArticulos/Program.cs
@@ -23,7 +23,7 @@ namespace registroArticulos
             int numArticulo, cantVendida;
             int [] totalCantidadVendida = new int[15]; //Cantidad de cada uno de los art
 
-            for(int i = 0; i < totalCantidadVendida.length ; i++){
+            for(int i = 0; i < 15 ; i++){
                 totalCantidadVendida[i] = 0; //Inicializa en 0 todos lo elementos
             }
 
@@ -34,7 +34,7 @@ namespace registroArticulos
             cantVendida = int.Parse(Console.ReadLine());
 
             while(numArticulo != 0){ //Mientras que sea distinto de 0... sigo pidiendo registro
-            totalCantidadVendida[numArticulo -1] += cantidad;  //numArticulo -1 por la posicion
+            totalCantidadVendida[numArticulo -1] += cantVendida;  //numArticulo -1 por la posicion
                 //le incremento a ese indice la cantidad en 1
     
             Console.WriteLine("Ingrese el numero de articulo: ");
@@ -47,7 +47,7 @@ namespace registroArticulos
             //Punto A: tengo q buscar el maximo del vector y su posicion (indice + 1: el art)
             int maxCantidad = totalCantidadVendida[0];   //Asigno el maximo en el primer elemento para arrancar
             int numMaximo = 1;   //Asigno el numero de art. en 1 para arrancar
-            for(int i = 0 ; i < totalCantidadVendida.length ; i++){
+            for(int i = 0 ; i < 15 ; i++){
                 if (totalCantidadVendida[i] > maxCantidad) {   
                     maxCantidad = totalCantidadVendida[i];     //Le asigno el actual
                     numArticulo = i + 1;     //Le digo q mi articulo va a ser ese + 1 
@@ -57,12 +57,11 @@ namespace registroArticulos
         
         
             //Punto B: Numero de los productos q no tienen ventas
-            for (int i = 0; i < totalCantidadVendida.length ; i++){
-                if(totalCantidadVendida[x] == 0){   //Si alguno del array de cantidades tiene 0...
-                    Console.WriteLine("El producto " + (x+1) + " no registró ventas");
+            for (int i = 0; i < 15 ; i++){
+                if(totalCantidadVendida[i] == 0){   //Si alguno del array de cantidades tiene 0...
+                    Console.WriteLine("El producto " + (i+1) + " no registró ventas");
                 }
             }
-
             //Punto C: Cuantos hay del punto C
             Console.WriteLine("La cantidad del 10 es: " + totalCantidadVendida[9]);   
         }
