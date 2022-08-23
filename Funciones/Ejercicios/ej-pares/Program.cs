1d0314b9dce44c8e77c92638358e54b0080373bc
cecigiunta <ceciliagiunta57@gmail.com>
2022-08-23 15:56:39 -0300

    ej funciones-npares

--- Funciones/Ejercicios/ej-pares/Program.cs
+++ Funciones/Ejercicios/ej-pares/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Hacer una función llamada “par” que reciba un número entero y devuelva 1 si es par o cero si no lo es. 
+// Hacer un programa para ingresar 20 números y mostrar por pantalla cuántos son pares.
+
+
+using System;
 
 namespace ej_pares
 {
@@ -6,7 +10,26 @@ namespace ej_pares
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero, cantPares, totalPares = 0;
+            for(int i = 0 ; i < 5 ; i++){
+                Console.WriteLine("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                cantPares = par(numero);
+                if(cantPares == 1){
+                    totalPares++;
+                }
+            }
+            Console.WriteLine("La cantidad de pares que ingresaste es de: " + totalPares);
+
+        }
+
+        static int par(int n){
+            if(n % 2 == 0){
+                return 1 ;
+            } else {
+                return 0 ;
+            }
         }
     }
 }
